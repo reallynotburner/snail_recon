c4d6d98dfd13575f0f9db62fa52f37228c6ece44
reallynotburner <reallynotburner@gmail.com>
2019-02-09 11:43:50 -0800

    added timestamp to images

--- timelapse.py
+++ timelapse.py
@@ -2,6 +2,7 @@ from gpiozero import Button, LED
 from time import sleep, time
 from picamera import PiCamera
 import subprocess
+import datetime as dt
 
 userButton = Button(2)
 red = LED(17)
@@ -9,24 +10,26 @@ irLED = LED(27)
 camera = PiCamera()
 camera.resolution = (1280, 720)
 camera.color_effects = (128,128)
-numberOfCaptures = 25200 
+numberOfCaptures = 10 
 secondsInterval = 1
 timeOfCapture = 0
 
 irLED.on()
 def wait(timeOfCapture):
     waitTime = 1 - time() + timeOfCapture;
-    print('toc wait', waitTime)
     if waitTime > 0:
         sleep(waitTime)
         
 def getStill(i):
     red.on()
+    camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     camera.capture(
         'image{0:06d}.jpg'.format(i),
         quality=10,
         )
     red.off()
+    print('captured ', i, ' of ', numberOfCaptures, ' images.')
+
 
 for i in range(numberOfCaptures):
     timeOfCapture = time()
